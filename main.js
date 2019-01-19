d3e3e774f3bc56cf3221fc5c98badba43eb9b954
Toyin Dawudu <toyindawudu@gmail.com>
2019-01-19 20:00:33 +0000

    Studying JS basics

---
+++ main.js
@@ -0,0 +1,44 @@
+// Type coercion
+
+var firstName = 'Josiah';
+var age = 2;
+var sex = 'male';
+
+console.log(firstName + ' is ' + age + ' years old' + '.')
+
+var job, isMarried;
+job = 'Student';
+isMarried = "false"
+
+console.log(firstName + ' is ' + age + ' years old' + '. ' + 'He is a ' + job) 
+
+//Variable mutation
+
+age = 'twenty-eight';
+console.log(firstName + ' is ' + age)
+
+//Basic Operators
+
+var now = 2019;
+ageJosiah = 2;
+ageToyin = 33;
+ageTiti = 38;
+
+// Maths Operators
+
+yearJosiah = now - ageJosiah;
+yearToyin = now - ageToyin;
+yearTiti = now - ageTiti;
+
+
+console.log (yearJosiah + 2);
+console.log (yearToyin - 3);
+console.log (yearTiti + 5);
+
+//Logical operators
+var titiOlder = ageTiti > ageToyin;
+console.log(titiOlder);
+
+
+//typeof operator
+
