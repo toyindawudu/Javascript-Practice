6bde14870c065cf400597111f456ea45f0ee3811
Toyin Dawudu <toyindawudu@gmail.com>
2019-02-26 16:48:38 +0000

    Still working on learning more JS

--- main.js
+++ main.js
@@ -665,7 +665,75 @@ function maxHeartRate (el) {
   
 }
 
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
 
 // console.log(ages, fullAges);
+
+// First Coding Challenge
+
+// function charFreq(message) {
+
+//   // var sentence = [];
+//   // message[i] = 0;
+
+//   if (message.length > -1) {
+//     var i = 0;
+//     var sentence = [];
+//     while (i <= message.length + 1) {
+//       sentence.push (message[i]);
+//       i++;
+//     }
+  
+//   // sentence = word.push(message[i])
+//   // return word;
+//   } else {
+//     return 'Thanks';
+//   }
+
+// }
+  
+// charFreq('Toyin');
+
+// Function returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function (name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+  return function (name) {
+    console.log('What subject do you teach, ' + name + '?');
+  }
+} else {
+  return function (name) {
+    console.log('Hello' + name + ', what do you do?');
+  }
+}}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion ('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+
+// Immediately Invoked Function Expressions (IIFE)
+
+// function game () {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// } 
+
+// game();
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+(function(goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
